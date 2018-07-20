ab3dc96a39a973715e486c9d86c65ac08dc3a11b
eguradi <gururaj.adiga@ericsson.com>
2018-07-20 08:32:24 -0400

    home page final

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../product.service';
 import { Product } from '../models/product.model';
-import { CategoryService } from '../category.service';
 import { Observable } from 'rxjs/Observable';
 import { filter, switchMap } from 'rxjs/operators';
 import { ActivatedRoute } from '@angular/router';
@@ -16,62 +15,49 @@ export class ProductsComponent implements OnInit {
 
   productList$: Observable<any[]>;
   productList: Product[];
-  items: Product[]=[];
-  categories$: Observable<any[]>;
-  categoryList: {}[] = [];
+  items: Product[] = [];
   selectedCategory: string;
 
   constructor(
     private productService: ProductService,
-    private categoryService: CategoryService,
     private activatedRoute: ActivatedRoute
   ) {
-    this.activatedRoute.queryParamMap.subscribe(param=>this.selectedCategory = param.get('category') || null);
-    this.categories$ = this.categoryService.categories;
-    let cats=[];
-    this.categories$.subscribe(catList=>{
-      this.categoryList = cats;
-      catList.forEach(proCats => {
-        let indx = cats.length;
-        cats[indx]={};
-        cats[indx]['key'] = proCats.key;
-        cats[indx]['name'] = proCats.payload.val().name;
-      });
-    });
+    // this.activatedRoute.queryParamMap.subscribe(param => this.selectedCategory = param.get('category') || null);
+
 
     this.productList$ = this.productService.products;
-    let prods=[];
+    let prods = [];
 
-    let subsc: Subscription = this.productList$.pipe(switchMap(prodList=>{
-        this.productList = prods;
-        let selectCat = this.selectedCategory;
-        prodList.forEach(products => {
-          let indx = prods.length;
-          prods[indx]={};
-          prods[indx]['key'] = products.key;
-          prods[indx]['title'] = products.payload.val().title;
-          prods[indx]['price'] = products.payload.val().price;
-          prods[indx]['imageUrl'] = products.payload.val().imageUrl;
-          prods[indx]['category'] = products.payload.val().category;
-        });
-        return this.activatedRoute.queryParamMap;
-      })
-    ).subscribe(param=>{
+    let subsc: Subscription = this.productList$.pipe(switchMap(prodList => {
+      this.productList = prods;
+      let selectCat = this.selectedCategory;
+      prodList.forEach(products => {
+        let indx = prods.length;
+        prods[indx] = {};
+        prods[indx]['key'] = products.key;
+        prods[indx]['title'] = products.payload.val().title;
+        prods[indx]['price'] = products.payload.val().price;
+        prods[indx]['imageUrl'] = products.payload.val().imageUrl;
+        prods[indx]['category'] = products.payload.val().category;
+      });
+      return this.activatedRoute.queryParamMap;
+    })
+    ).subscribe(param => {
       this.selectedCategory = param.get('category') || null;
       this.filter(this.selectedCategory);
-      subsc.unsubscribe();
+      // subsc.unsubscribe();
     });
   }
 
   ngOnInit() {
-    
+
   }
-  
+
   filter(query: any) {
     this.selectedCategory = query ? query : null;
     this.items = query ?
-     this.productList.filter(product=>product.category === query):
-     this.productList;
+      this.productList.filter(product => product.category === query) :
+      this.productList;
   }
 
 }
