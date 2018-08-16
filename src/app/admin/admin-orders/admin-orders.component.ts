e43fdc7a779185b4b6f33e7c9fb79d96b48b0af0
eguradi <gururaj.adiga@ericsson.com>
2018-08-16 13:35:39 -0400

    updated admin order list

--- src/app/admin/admin-orders/admin-orders.component.ts
+++ src/app/admin/admin-orders/admin-orders.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { OrdersService } from '../../orders.service';
 
 @Component({
   selector: 'app-admin-orders',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminOrdersComponent implements OnInit {
 
-  constructor() { }
+  private orders$;
+  myOrders: any[];
+  
+  constructor(private orderService: OrdersService) {
+    this.orders$ = this.orderService.orders;
+   }
 
   ngOnInit() {
+    this.orders$.subscribe(orders=>{
+      console.log('orders', orders)
+      this.myOrders = orders
+      this.myOrders.forEach(order=>{
+        let totPrice: number=0;
+        order.items.forEach(item=>{
+          totPrice += item.totalPrice;
+        });
+        order['totalPrice'] = totPrice;
+      })
+    });
   }
 
 }
