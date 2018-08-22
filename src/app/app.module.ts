91f4cd25feb57db1ba92ca848cd7ad1844740b40
eguradi <gururaj.adiga@ericsson.com>
2018-08-22 14:50:55 -0400

    added service worker support

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,21 @@
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { DataTableModule } from 'angular5-data-table';
 import { AngularFireModule, FirebaseAppConfigToken } from 'angularfire2';
-import { AngularFireAuth, AngularFireAuthModule } from 'angularfire2/auth';
-import { AngularFireDatabaseModule } from 'angularfire2/database';
-import { CustomFormsModule } from 'ng2-validation';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { ProductsResolve } from 'shared/resolvers/product-resolve.service';
 
 import { environment } from '../environments/environment';
 import { AdminModule } from './admin/admin.module';
 import { AppComponent } from './app.component';
-import { BsNavbarComponent } from './core/components/bs-navbar/bs-navbar.component';
 import { ContainerTestComponent, Pane } from './container-test/container-test.component';
 import { ContentTestComponent } from './content-test/content-test.component';
-import { HomeComponent } from './core/components/home/home.component';
 import { LoginComponent } from './core/components/login/login.component';
-import { SharedModule } from './shared/shared.module';
-import { ShoppingModule } from './shopping/shopping.module';
-import { ProductsComponent } from './shopping/components/products/products.component';
-import { ProductsResolve } from 'shared/resolvers/product-resolve.service';
 import { CoreModule } from './core/core.module';
+import { SharedModule } from './shared/shared.module';
+import { ProductsComponent } from './shopping/components/products/products.component';
+import { ShoppingModule } from './shopping/shopping.module';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [
@@ -46,7 +40,8 @@ import { CoreModule } from './core/core.module';
     SharedModule,
     AdminModule,
     ShoppingModule,
-    CoreModule
+    CoreModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [
     {provide: FirebaseAppConfigToken, useValue: environment.firebase},
